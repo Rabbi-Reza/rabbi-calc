dc41ab93af7b65dcfb48b789ef0c39d39d3a7c3e
Rabbi-Reza <rabbi.cse1.bu@gmail.com>
2022-09-28 17:25:55 +0600

    AddTwo, CheckEven

---
+++ index.js
@@ -0,0 +1,37 @@
+let firstNumGL = 0
+let secondNumGL = 0
+
+const AddTwo = (firstNum, secondNum) => {
+    firstNumGL = firstNum
+    secondNumGL = secondNum
+
+    return firstNum + secondNum;
+}
+
+const CheckEven = (input) => {
+    if(input === 0)
+        return true;
+    else if (input % 2 === 0) {
+        return true;
+    }
+    else if (input % 2 !== 0) {
+        return false;
+    }
+}
+
+// const ViewResultAdd = () => {
+//     var tag = document.createElement("p"); // <p></p>
+//     var text = document.createTextNode(firstNumGL + secondNumGL); 
+//     tag.appendChild(text);
+//     // var element = document.getElementsByTagName("body")[0];
+//     var element = document.getElementsByTagName("div");
+//     // var element = document.getElementsByClassName("add-show")
+//     element.appendChild(tag); 
+// }
+
+module.exports = {
+    AddTwo,
+    CheckEven, 
+    // ViewResultAdd,
+    
+}
